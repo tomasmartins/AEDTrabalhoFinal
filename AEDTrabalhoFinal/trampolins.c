3912de4e34a78b5abe005b0f4ffa8c2adb11e8d6
tomasmartins <TRM@TRMMacbook13.local>
2016-05-13 17:57:20 +0100

    Merda de dicionario!!!

---
+++ AEDTrabalhoFinal/trampolins.c
@@ -0,0 +1,9 @@
+//
+//  trampolins.c
+//  AEDTrabalhoFinal
+//
+//  Created by Tomás Martins on 13/05/16.
+//  Copyright © 2016 Tomás Martins & Miguel Real. All rights reserved.
+//
+
+#include "trampolins.h"
---
+++ AEDTrabalhoFinal/trampolins.h
@@ -0,0 +1,14 @@
+//
+//  trampolins.h
+//  AEDTrabalhoFinal
+//
+//  Created by Tomás Martins on 13/05/16.
+//  Copyright © 2016 Tomás Martins & Miguel Real. All rights reserved.
+//
+
+#ifndef trampolins_h
+#define trampolins_h
+
+#include <stdio.h>
+
+#endif /* trampolins_h */
