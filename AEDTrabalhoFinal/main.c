a43dbfd3de18538e859616488f21479a1c9d8e76
tomasmartins <TRM@trmmacbook13.local>
2016-05-13 16:03:06 +0100

    Initial Commit

---
+++ AEDTrabalhoFinal/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AEDTrabalhoFinal
+//
+//  Created by Tomás Martins on 13/05/16.
+//  Copyright © 2016 Tomás Martins & Miguel Real. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
